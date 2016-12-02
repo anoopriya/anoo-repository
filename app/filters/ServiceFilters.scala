721956d578d711aee93259388703cfa360a12e85
Juan Ricardo Enriquez Torres <juanricardo_enriqueztorres@s5a.com>
2016-12-02 08:33:01 -0500

    add correlation_id functionality to template

--- app/filters/ServiceFilters.scala
+++ app/filters/ServiceFilters.scala
@@ -4,11 +4,14 @@ package filters
 import javax.inject.Inject
 import play.api.http.HttpFilters
 
+import monitoring.IdentifyRequestFilter
+
 // common logging and metrics for all requests
 class ServiceFilters @Inject() (
-    timing:    TimingFilter,
-    increment: IncrementFilter,
-    exception: ExceptionFilter
+    identifyRequest: IdentifyRequestFilter,
+    timing:          TimingFilter,
+    increment:       IncrementFilter,
+    exception:       ExceptionFilter
 ) extends HttpFilters {
-  val filters = Seq(timing, increment, exception)
+  val filters = Seq(identifyRequest, timing, increment, exception)
 }
